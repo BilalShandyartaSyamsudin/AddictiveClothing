ee3b36034b3f3f20dd0f6a8ea19ba17cd870e99f
Bilal Shandyarta Syamsudin <e41212037@student.polije.ac.id>
2023-05-26 22:29:08 +0700

    API UNTUK MENAMPILKAN PRODUK UPDATE

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\ApiAuthController;
 use App\Http\Controllers\API\ApiRegisterController;
+use App\Http\Controllers\API\ApiProdukController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
@@ -11,6 +12,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('/login', [ApiAuthController::class, 'login']);
 Route::post('/register', [ApiRegisterController::class, 'store']);
+Route::get('/produk', [ApiProdukController::class, 'produk']);
 
 Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [ApiAuthController::class, 'logout']);
